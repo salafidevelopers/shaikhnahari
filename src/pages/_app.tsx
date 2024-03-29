a66e52c75d5b26117904558e65749b88e65bd5bb
iamluqmaan <19-52hl070@students.unilorin.edu.ng>
2024-03-29 11:57:57 +0100

    remove next_ui configurations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { NextUIProvider } from "@nextui-org/react";
+
 import { Alexandria } from "next/font/google";
 
 const alexandria = Alexandria({
@@ -10,10 +10,10 @@ const alexandria = Alexandria({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    
       <main className={`${alexandria.variable} font-alexandria`}>
         <Component {...pageProps} />
       </main>
-    </NextUIProvider>
+    
   );
 }
