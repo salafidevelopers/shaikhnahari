de029190436b5fc75244bb0388a149d6e5941ba4
AOMuiz <abdulwasiumuiz@gmail.com>
2024-03-31 06:18:12 +0100

    feat: convert hero to a component and move navbar to global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Alexandria } from "next/font/google";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const alexandria = Alexandria({
   subsets: ["latin"],
@@ -9,8 +12,16 @@ const alexandria = Alexandria({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${alexandria.variable} font-alexandria`}>
-      <Component {...pageProps} />
-    </main>
+    <Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Sheikh Yahyah An-Nahari</title>
+      </Head>
+      <Navbar />
+      <main className={`${alexandria.variable} font-alexandria`}>
+        <Component {...pageProps} />
+      </main>
+    </Fragment>
   );
 }
