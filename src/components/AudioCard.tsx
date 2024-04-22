7192c9d7c1402fe961c92f5229fd13474e05e98a
sheriff <ajibolasheriffdeen02@gmail.com>
2024-04-22 18:43:34 +0100

    chore

--- src/components/AudioCard.tsx
+++ src/components/AudioCard.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { RiDownloadCloud2Line } from "react-icons/ri";
 
-const AudioCard = () => {
+const AudioCard = ({ size }: { size: string }) => {
   return (
-    <div className="p-2 flex bg-primary-200 gap-2">
-      <div>wed,25 oct</div>
-      <div className="border border-x-2 py-1.5  px-3 bg-primary-400 rounded-md ">
-        download
-      </div>
-      <div className=""> scroll</div>
-      <div>knowledge and its virtues</div>
+    <div className="font-alexandria p-2 flex bg-primary-200 gap-2 items-center text-primary-700 justify-between">
+      <div className="font-semibold text-[18px]">العلم: فضله وشرفُ أهله</div>
+      <div className="flex-grow bg-white rounded-md px-2 py-1">slider</div>
+      <button className="border border-x-2 py-2 font-bold flex gap-2.5 px-3 bg-primary-400 rounded-md  items-center">
+        <span className="font-bold text-[12px] ">Download</span>
+        <span className="text-2xl font-bold">
+          <RiDownloadCloud2Line />
+        </span>
+      </button>
+      <div className="text-[12px] font-bold">بتاريخ : 21/يوليو/2015</div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import AudioCard from "@/components/AudioCard";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <section className="my-5 p-5">
+        <AudioCard size="lg" />
+      </section>
+    </>
   );
 }
