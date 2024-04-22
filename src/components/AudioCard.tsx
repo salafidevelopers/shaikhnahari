e65f9166864a396cddf20fe17974aae92f6c84f4
sheriff <ajibolasheriffdeen02@gmail.com>
2024-04-22 17:17:08 +0100

    create audioplayer skeleter component

---
+++ src/components/AudioCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AudioCard = () => {
+  return (
+    <div className="p-2 flex bg-primary-200 gap-2">
+      <div>wed,25 oct</div>
+      <div className="border border-x-2 py-1.5  px-3 bg-primary-400 rounded-md ">
+        download
+      </div>
+      <div className=""> scroll</div>
+      <div>knowledge and its virtues</div>
+    </div>
+  );
+};
+
+export default AudioCard;
