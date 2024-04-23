d8e8080a06c2c6eae6ec589f3fa11ebbf00bc535
Lukman10a <abdulrauflukman9@gmail.com>
2024-04-23 17:22:11 +0100

    create the biography component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { BiographyPage } from "@/components/biograpghy";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <BiographyPage />
     </div>
   );
 }
