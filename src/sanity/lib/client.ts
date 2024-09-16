bd2bce8f7d3f7ead9c7624bae588ca2880309ebb
iamluqmaan <19-52hl070@students.unilorin.edu.ng>
2024-09-16 11:28:13 +0100

    initialized connection with sanity

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,6 +1,9 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+ const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-09-01'; // Default to a specific version if not set
+
 
 export const client = createClient({
   projectId,
